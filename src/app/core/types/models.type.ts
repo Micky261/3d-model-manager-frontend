f9d5225d16db90ebc3e9078810ea3c8aefc0f086
Frank Nelles <f.nelles@hotmail.com>
2021-01-09 20:54:21 +0100

    Add: Fields to Models (available in backend)

--- src/app/core/types/models.type.ts
+++ src/app/core/types/models.type.ts
@@ -2,10 +2,19 @@ export class Models {
     constructor(
         public id: number,
         public name: string,
-        public imported_name: string,
         public links: string[],
+        public description: string,
+        public notes: string,
+        public favorite: boolean,
+        public author: string,
+        public licence: string,
         public created_at: string,
-        public updated_at: string
+        public updated_at: string,
+        public imported_name?: string,
+        public import_source?: string,
+        public imported_description?: string,
+        public imported_author?: string,
+        public imported_licence?: string,
     ) {
     }
 }
