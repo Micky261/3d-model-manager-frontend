80170ae42bab012f138defbd624f6ce766e71a8b
Frank Nelles <f.nelles@hotmail.com>
2021-01-23 12:05:00 +0100

    Add: Services

--- src/app/core/model-files.service.ts
+++ src/app/core/model-files.service.ts
@@ -1,7 +1,9 @@
 import {HttpClient} from "@angular/common/http";
 import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
 import {Configuration} from "../../configuration";
 import {ChunkedUploadService} from "./chunked-upload.service";
+import {ModelFile} from "./types/model-file.type";
 
 @Injectable({
     providedIn: "root"
@@ -17,5 +19,20 @@ export class ModelFilesService {
         return ChunkedUploadService.chunkedUpload(
             file, file.name, this.httpClient, `${this.apiUrl}/model/file/${encodeURIComponent(String(modelId))}`, type, progress
         );
+    }
+
+    getFiles(modelId: number, type: string): Observable<ModelFile[]> {
+        return this.httpClient.get<ModelFile[]>(`${this.apiUrl}/model/files/${encodeURIComponent(String(modelId))}/${encodeURIComponent(String(type))}`);
+    }
+
+    getFileUrl(modelId: number, type: string, filename: string): string {
+        return `${this.apiUrl}/model/file/${encodeURIComponent(String(modelId))}/${encodeURIComponent(String(filename))}/${encodeURIComponent(String(type))}`;
+    }
+
+    getFile(modelId: number, type: string, filename: string): Observable<ModelFile[]> {
+        const m = encodeURIComponent(String(modelId));
+        const f = encodeURIComponent(String(filename));
+        const t = encodeURIComponent(String(type));
+        return this.httpClient.get<ModelFile[]>(`${this.apiUrl}/model/file/${m}/${f}/${t}`);
     }
 }
