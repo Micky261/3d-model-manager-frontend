9efe9725df4253d967de0ae9cd14028c4ae05206
Frank Nelles <f.nelles@hotmail.com>
2021-03-01 11:43:54 +0100

    Add: Show error if file target exists (file list update)

--- src/app/core/types/serverMessage.type.ts
+++ src/app/core/types/serverMessage.type.ts
@@ -2,7 +2,8 @@ export class ServerMessage {
     constructor(
         public message: string,
         public message_code: string,
-        public model_id?: number
+        public model_id?: number,
+        public additional_information?: string[],
     ) {
     }
 }
--- src/app/model/files-list-element/files-list-element.component.ts
+++ src/app/model/files-list-element/files-list-element.component.ts
@@ -1,7 +1,10 @@
 import {Component, Inject, Input, OnInit} from "@angular/core";
-import {L10N_LOCALE, L10nLocale} from "angular-l10n";
+import {L10N_LOCALE, L10nLocale, L10nTranslationService} from "angular-l10n";
+import {ToastrService} from "ngx-toastr";
+import {ToastService} from "../../core/error/toast.service";
 import {ModelFilesService} from "../../core/services/model-files.service";
 import {ModelFile} from "../../core/types/model-file.type";
+import {ServerMessage} from "../../core/types/serverMessage.type";
 
 @Component({
     selector: "app-files-list-element",
@@ -17,6 +20,9 @@ export class FilesListElementComponent implements OnInit {
     constructor(
         @Inject(L10N_LOCALE) public locale: L10nLocale,
         private readonly modelFilesService: ModelFilesService,
+        private readonly toast: ToastService,
+        private readonly toastr: ToastrService,
+        private readonly translation: L10nTranslationService
     ) {
     }
 
@@ -25,8 +31,26 @@ export class FilesListElementComponent implements OnInit {
     }
 
     saveFileList(): void {
-        this.modelFilesService.updateFiles(this.modelId, this.files).subscribe(() =>
-            this.lastSaved = Date.now()
+        this.modelFilesService.updateFiles(this.modelId, this.files).subscribe(
+            files => {
+                this.files = files;
+                this.lastSaved = Date.now();
+            },
+            error => {
+                const sMsg = error.error as ServerMessage;
+                this.toast.showBackendError(sMsg.message_code);
+
+                if (sMsg.additional_information) {
+                    const oldType = sMsg.additional_information[0] ;
+                    const oldTypeName = this.translation.translate("filetype." + oldType) as string;
+                    const newType = sMsg.additional_information[1] ;
+                    const newTypeName = this.translation.translate("filetype." + newType) as string;
+                    const filename = sMsg.additional_information[2] ;
+
+                    const change = this.translation.translate("ProblematicFile") as string;
+                    this.toastr.info("" + oldTypeName + " -> " + newTypeName + "<br/>" + filename, change, {enableHtml: true});
+                }
+            }
         );
     }
 }
