21d4cb78fa33d1fd09497dace125f43fb19cb897
Frank Nelles <f.nelles@hotmail.com>
2021-03-25 13:34:22 +0100

    Add: Better sorting for object arrays with multiple descending field settings

--- src/app/core/pipes/object-array-sort.pipe.ts
+++ src/app/core/pipes/object-array-sort.pipe.ts
@@ -11,19 +11,29 @@ export class ObjectArraySortPipe implements PipeTransform {
      * @param field Single field as string or multiple fields as array of strings
      * @param descending Sort descending (defaults to false)
      */
-    transform<T>(array: T[], field: string | string[], descending = false): T[] {
+    transform<T>(array: T[], field: string | string[], descending: boolean | boolean[] = false): T[] {
         if (!Array.isArray(array)) {
             return;
         }
         const fields: string[] = (!Array.isArray(field)) ? [field] : field;
+        const descendings: boolean[] = (!Array.isArray(descending)) ? [descending] : descending;
 
-        let descSwitch = 0;
-        if (descending === true) {
-            descSwitch = 2;
+        if (fields.length === 0) {
+            return array;
         }
 
         array.sort((a: any, b: any) => {
             for (let i = 0; i < fields.length; i++) {
+                let descSwitch = 0;
+                if (descendings.length === 1) {
+                    // Only one given -> Apply to all
+                    descSwitch = (descendings[0]) ? 2 : 0;
+                } else {
+                    // Multiple ones given -> Apply to corresponding field
+                    // If too less given it defaults to ascending
+                    descSwitch = (descendings[i]) ? 2 : 0;
+                }
+
                 if (a[fields[i]] < b[fields[i]]) {
                     return descSwitch - 1;
                 } else if (a[fields[i]] > b[fields[i]]) {
