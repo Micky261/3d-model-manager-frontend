e737e6d2e9cc539e39de3c05733f2326cdea44fc
Frank Nelles <f.nelles@hotmail.com>
2022-03-10 16:16:31 +0100

    Angular 10 → 11

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,33 +3,40 @@ import {RouterModule} from "@angular/router";
 import {AuthGuardService} from "./core/auth/auth-guard.service";
 
 @NgModule({
-    imports: [RouterModule.forRoot([
-        {
-            path: "",
-            loadChildren: () => import("./home/home.module").then(m => m.HomeModule),
-            canActivate: [AuthGuardService]
-        },
-        {
-            path: "static",
-            loadChildren: () => import("./static/static.module").then(m => m.StaticModule)
-        },
-        {
-            path: "login",
-            loadChildren: () => import("./login/login.module").then(m => m.LoginModule)
-        },
-        {
-            path: "register",
-            loadChildren: () => import("./register/register.module").then(m => m.RegisterModule)
-        },
-        {
-            path: "email-resend",
-            loadChildren: () => import("./email-resend/email-resend.module").then(m => m.EmailResendModule)
-        },
-        {
-            path: "model",
-            loadChildren: () => import("./model/model.module").then(m => m.ModelModule)
-        }
-    ])],
+    imports: [
+        RouterModule.forRoot(
+            [
+                {
+                    path: "",
+                    loadChildren: () => import("./home/home.module").then(m => m.HomeModule),
+                    canActivate: [AuthGuardService]
+                },
+                {
+                    path: "static",
+                    loadChildren: () => import("./static/static.module").then(m => m.StaticModule)
+                },
+                {
+                    path: "login",
+                    loadChildren: () => import("./login/login.module").then(m => m.LoginModule)
+                },
+                {
+                    path: "register",
+                    loadChildren: () => import("./register/register.module").then(m => m.RegisterModule)
+                },
+                {
+                    path: "email-resend",
+                    loadChildren: () => import("./email-resend/email-resend.module").then(m => m.EmailResendModule)
+                },
+                {
+                    path: "model",
+                    loadChildren: () => import("./model/model.module").then(m => m.ModelModule)
+                }
+            ],
+            {
+                relativeLinkResolution: "legacy"
+            }
+        )
+    ],
     exports: [
         RouterModule
     ]
