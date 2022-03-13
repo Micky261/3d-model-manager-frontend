7ccf693f17c7514e459ead8be37ae1aa7e39cf6f
Frank Nelles <f.nelles@hotmail.com>
2022-03-13 17:59:53 +0100

    Add: Deletion and download of single files through file list

--- src/app/model/files-list-element/files-list-element.component.ts
+++ src/app/model/files-list-element/files-list-element.component.ts
@@ -1,6 +1,8 @@
 import {Component, Inject, Input, OnInit} from "@angular/core";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 import {L10N_LOCALE, L10nLocale, L10nTranslationService} from "angular-l10n";
 import {ToastrService} from "ngx-toastr";
+import "../../../shared/array.extension";
 import {ToastService} from "../../core/error/toast.service";
 import {ModelFilesService} from "../../core/services/model-files.service";
 import {ModelFile} from "../../core/types/model-file.type";
@@ -16,13 +18,15 @@ export class FilesListElementComponent implements OnInit {
 
     files: ModelFile[] = [];
     lastSaved: number;
+    fileForDeletion: ModelFile;
 
     constructor(
         @Inject(L10N_LOCALE) public readonly locale: L10nLocale,
         private readonly modelFilesService: ModelFilesService,
         private readonly toast: ToastService,
         private readonly toastr: ToastrService,
-        private readonly translation: L10nTranslationService
+        private readonly translation: L10nTranslationService,
+        public readonly modalService: NgbModal
     ) {
     }
 
@@ -41,16 +45,48 @@ export class FilesListElementComponent implements OnInit {
                 this.toast.showBackendError(sMsg.message_code);
 
                 if (sMsg.additional_information) {
-                    const oldType = sMsg.additional_information[0] ;
+                    const oldType = sMsg.additional_information[0];
                     const oldTypeName = this.translation.translate("filetype." + oldType) as string;
-                    const newType = sMsg.additional_information[1] ;
+                    const newType = sMsg.additional_information[1];
                     const newTypeName = this.translation.translate("filetype." + newType) as string;
-                    const filename = sMsg.additional_information[2] ;
+                    const filename = sMsg.additional_information[2];
 
                     const change = this.translation.translate("ProblematicFile") as string;
                     this.toastr.info("" + oldTypeName + " -> " + newTypeName + "<br/>" + filename, change, {enableHtml: true});
                 }
             }
         );
+    }
+
+    downloadFile(file: ModelFile): void {
+        this.modelFilesService.getFile(file.model_id, file.type, file.filename).subscribe(
+            data => {
+                // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+                const blob = new Blob([data]);
+                const url = window.URL.createObjectURL(blob);
+                window.open(url);
+            },
+            error => {
+                const sMsg = error.error as ServerMessage;
+                this.toast.showBackendError(sMsg.message_code);
+
+                if (sMsg.additional_information) {
+                    const type = sMsg.additional_information[0];
+                    const typeName = this.translation.translate("filetype." + type) as string;
+                    const filename = sMsg.additional_information[1];
+
+                    const change = this.translation.translate("ProblematicFile") as string;
+                    this.toastr.info(`${filename} (${typeName})`, change, {enableHtml: true});
+                }
+            }
+        );
+    }
+
+    deleteFile(): void {
+        this.modelFilesService.deleteFile(this.fileForDeletion.model_id, this.fileForDeletion.type, this.fileForDeletion.filename).subscribe(() => {
+            this.toast.showSuccess("FileDeleted");
+            this.files.remove(this.fileForDeletion);
+            this.fileForDeletion = undefined;
+        });
     }
 }
