5d6f22ad0bcb1dd0ece586a45d9461a4f4a9edb0
Frank Nelles <f.nelles@hotmail.com>
2022-08-22 15:19:52 +0200

    Add: Standard case for tags

--- src/app/core/pipes/custom-pipes.module.ts
+++ src/app/core/pipes/custom-pipes.module.ts
@@ -1,17 +1,20 @@
 import {NgModule} from "@angular/core";
 import {ArraySortPipe} from "./array-sort.pipe";
 import {ObjectArraySortPipe} from "./object-array-sort.pipe";
+import {PascalCasePipe} from "./pascal-case.pipe";
 import {SecurePipe} from "./secure.pipe";
 
 @NgModule({
     declarations: [
         ObjectArraySortPipe,
         ArraySortPipe,
+        PascalCasePipe,
         SecurePipe
     ],
     exports: [
         ObjectArraySortPipe,
         ArraySortPipe,
+        PascalCasePipe,
         SecurePipe
     ]
 })
---
+++ src/app/core/pipes/pascal-case.pipe.ts
@@ -0,0 +1,13 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+    name: "pascalCase"
+})
+export class PascalCasePipe implements PipeTransform {
+    transform(s: string): string {
+        return s.replace(/(\w)(\w*)/g,
+            function (g0: string, g1: string, g2: string) {
+                return g1.toUpperCase() + g2;
+            });
+    }
+}
