accd03d6791904b38f3b158c1ef23fc5d1f2e808
Frank Nelles <f.nelles@hotmail.com>
2023-11-12 15:16:24 +0100

    Update Angular 16->17

--- src/app/core/pipes/filesize.pipe.ts
+++ src/app/core/pipes/filesize.pipe.ts
@@ -1,11 +1,11 @@
 import {Pipe, PipeTransform} from "@angular/core";
-import * as fsize from "filesize";
+import {filesize} from "filesize";
 
 @Pipe({
     name: "filesize"
 })
 export class FileSizePipe implements PipeTransform {
-    transform(value: number, options?: any) {
-        return fsize(value, options);
+    transform(value: number) {
+        return filesize(value);
     }
 }
